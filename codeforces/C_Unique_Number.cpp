d2ad6d2cc9e77b09688937209bdef3413525ee62
Aaris Ahmed <aaris.ahmed1999@gmail.com>
2021-07-08 18:48:03 +0530

    remove extra from uniqure number problem

--- codeforces/C_Unique_Number.cpp
+++ codeforces/C_Unique_Number.cpp
@@ -30,17 +30,17 @@ int main() {
         else if (n < 18)
             cout << n - 9 << 9 << nl;
         else if (n <= 24)
-            cout << n - 17 << 8 << 9 << nl;
+            cout << n - 17 << 89 << nl;
         else if (n <= 30)
-            cout << n - 24 << 7 << 8 << 9 << nl;
+            cout << n - 24 << 789 << nl;
         else if (n <= 35)
-            cout << n - 30 << 6 << 7 << 8 << 9 << nl;
+            cout << n - 30 << 6789 << nl;
         else if (n <= 39)
-            cout << n - 35 << 5 << 6 << 7 << 8 << 9 << nl;
+            cout << n - 35 << 56789 << nl;
         else if (n <= 42)
-            cout << n - 39 << 4 << 5 << 6 << 7 << 8 << 9 << nl;
+            cout << n - 39 << 456789 << nl;
         else if (n <= 44)
-            cout << n - 42 << 3 << 4 << 5 << 6 << 7 << 8 << 9 << nl;
+            cout << n - 42 << 3456789 << nl;
         else if (n == 45)
            cout << 123456789 << nl;
         else
