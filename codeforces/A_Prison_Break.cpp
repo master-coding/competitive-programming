033f5330fa65f25fce9902b6611eea8a311d5bbe
Aaris Ahmed <aaris.ahmed1999@gmail.com>
2021-07-09 18:25:44 +0530

    prison break added

---
+++ codeforces/A_Prison_Break.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define speedio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define tlp int tc; cin >> tc; while (tc--)
+#define rep(i, a, b) for(int i = a; i < b; ++i)
+#define sp ' '
+#define nl '\n'
+#define newline cout << '\n'
+#define ll long long int
+
+#define vi vector <int>
+#define pii pair <int, int>
+#define pb push_back
+#define pob pop_back
+#define f first
+#define s second
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+
+int main() {
+    speedio;
+    tlp {
+        int a, b;
+        cin >> a >> b;
+        cout << a * b << nl;
+    }   
+}
