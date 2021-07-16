49a82ae69af7b0cbfcc2d9e5a508daeca6294512
Aaris Ahmed <aaris.ahmed1999@gmail.com>
2021-07-16 20:49:11 +0530

    A_Cancel_the_Trains: create

---
+++ codeforces/A_Cancel_the_Trains.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define speedio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define tlp int tc; cin >> tc; while (tc--)
+#define rep(i, a, b) for(int i = a; i < b; ++i)
+#define sp ' '
+#define nl '\n'
+#define newline cout << '\n'
+#define ll long long int
+
+#define vi vector <int>
+#define pii pair <int, int>
+#define pb push_back
+#define pob pop_back
+#define f first
+#define s second
+#define yes cout << "YES" << nl 
+#define no cout << "NO" << nl
+
+int main() {
+    speedio;
+    tlp {
+        int n, m;
+        cin >> n >> m;
+        set <int> a, b; 
+        while(n--) {
+            int x;
+            cin >> x;
+            a.insert(x);
+        }
+        while(m--) {
+            int x;
+            cin >> x;
+            b.insert(x);
+        }
+        set<int> intersect;
+        set_intersection(a.begin(), a.end(), b.begin(), b.end(), inserter(intersect, intersect.begin()));
+        cout << intersect.size() << nl;
+    }   
+}
