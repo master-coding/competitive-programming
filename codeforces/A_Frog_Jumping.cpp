52ffb971a4d09ff784535eca3c0ac2baa48df04c
Aaris Ahmed <aaris.ahmed1999@gmail.com>
2021-07-17 22:11:04 +0530

    A_Frog_Jumping: create

---
+++ codeforces/A_Frog_Jumping.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define speedio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define tlp int tc; cin >> tc; while (tc--)
+#define rep(i, a, b) for(int i = a; i < b; ++i)
+#define sp ' '
+#define nl '\n'
+#define newline cout << '\n'
+#define ll long long int
+
+#define vi vector <int>
+#define pii pair <int, int>
+#define pb push_back
+#define pob pop_back
+#define f first
+#define s second
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+
+int main() {
+    speedio;
+    tlp {
+        ll a, b, k;
+        cin >> a >> b >> k;
+        cout << fixed << a * (int)ceil(((double)k/2)) -  b * (k/2) << nl; 
+    }   
+}
