e524f2bdea03a27af937031846c450054bc4596b
Aaris Ahmed <aaris.ahmed1999@gmail.com>
2021-07-28 19:47:16 +0530

    Leetcode/FindPeakElement: create

---
+++ Leetcode/FindPeakElement.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int low = 0, high = nums.size() - 1;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if ((mid == 0 || nums[mid - 1] <= nums[mid]) && (mid == nums.size() - 1 || nums[mid + 1] <= nums[mid]))
+                return mid;
+            else if (mid > 0 && nums[mid - 1] > nums[mid]) {
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        return low;
+    }
+};
