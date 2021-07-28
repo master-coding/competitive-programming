7cacd3cfeffd9033ac3b212506adfcfe73632a92
Aaris Ahmed <aaris.ahmed1999@gmail.com>
2021-07-28 20:50:31 +0530

    HackeRank/C++/arraysIntroduction: create

---
+++ HackeRank/C++/arraysIntroduction.cpp
@@ -0,0 +1,24 @@
+// https://www.hackerrank.com/challenges/arrays-introduction/problem
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i=0; i<n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    for (int i=n-1; i>=0; i--) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
