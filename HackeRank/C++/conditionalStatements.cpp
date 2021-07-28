22e7dae3e1a8518f7c48779437e61c595ea07651
Aaris Ahmed <aaris.ahmed1999@gmail.com>
2021-07-28 20:36:35 +0530

    HackeRank/C++/conditionalStatements: create

---
+++ HackeRank/C++/conditionalStatements.cpp
@@ -0,0 +1,20 @@
+// https://www.hackerrank.com/challenges/c-tutorial-conditional-if-else/copy-from/206787425
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    map <int, string> m = {{1, "one"}, {2, "two"}, {3, "three"}, {4, "four"}, {5, "five"}, {6, "six"},
+    {7, "seven"}, {8, "eight"}, {9, "nine"}};
+    if (n <= 9)
+        cout << m[n] << '\n';
+    else
+        cout << "Greater than 9" << '\n';
+
+    return 0;
+}
