7dedb557bc4a392fd705de405fde12d3b19e884e
Aaris Ahmed <aaris.ahmed1999@gmail.com>
2021-07-29 08:17:59 +0530

    HackeRank/C++/vectorSort: create

---
+++ HackeRank/C++/vectorSort.cpp
@@ -0,0 +1,22 @@
+// https://www.hackerrank.com/challenges/vector-sort/problem
+
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int n; cin >> n;
+    vector <int> v(n);
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+    for (int value : v) {
+        cout << value << ' ';
+    }
+    return 0;
+}
