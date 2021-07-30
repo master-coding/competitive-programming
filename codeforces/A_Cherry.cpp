2496febedaa2c1a85f49e70d3b09613cf3b08cac
Aaris Ahmed <aaris.ahmed1999@gmail.com>
2021-07-30 12:15:06 +0530

    Codeforces A_Cherry: create

---
+++ codeforces/A_Cherry.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define speedio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define tlp int tc; cin >> tc; while (tc--)
+#define rep(i, a, b) for(int i = a; i < b; ++i)
+#define sp ' '
+#define nl '\n'
+#define newline cout << '\n'
+#define ll long long int
+
+#define vi vector <int>
+#define pii pair <int, int>
+#define pb push_back
+#define pob pop_back
+#define f first
+#define s second
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+
+int main() {
+    speedio;
+    tlp {
+        int n; cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+        ll ans = 0;
+        rep(i, 1 , n) {
+            ans = max(ans, arr[i] * 1LL * arr[i - 1]);
+        }
+        cout << ans << nl; 
+    }
+}
