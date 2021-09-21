63c6a65e4ba0346b9eb39b4f8348a5862d9cc958
Aaris Ahmed <aaris.ahmed1999@gmail.com>
2021-09-21 18:04:53 +0530

    A_New_Year_and_the_Christmas_Ornament: create

---
+++ codeforces/A_New_Year_and_the_Christmas_Ornament.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define speedio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define tlp int tc; cin >> tc; while (tc--)
+#define rep(i, a, b) for(int i = a; i < b; ++i)
+#define sp ' '
+#define nl '\n'
+#define newline cout << '\n'
+#define ll long long int
+
+#define vi vector <int>
+#define pii pair <int, int>
+#define pb push_back
+#define pob pop_back
+#define f first
+#define s second
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+
+int main() {
+    speedio;
+    int y, b, r;
+    cin >> y >> b >> r;
+    cout << min(y + 2, min(b + 1, r)) * 3 - 3 << nl;
+}
