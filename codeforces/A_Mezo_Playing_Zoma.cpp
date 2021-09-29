8909a1b2fead5eb33902a0540aa188b19ffff4a0
Aaris Ahmed <aaris.ahmed1999@gmail.com>
2021-09-29 21:15:37 +0530

    1285A A_Mezo_Playing_Zoma: create

---
+++ codeforces/A_Mezo_Playing_Zoma.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define speedio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define tlp int tc; cin >> tc; while (tc--)
+#define rep(i, a, b) for(int i = a; i < b; ++i)
+#define sp ' '
+#define nl '\n'
+#define newline cout << '\n'
+#define ll long long int
+
+#define vi vector <int>
+#define pii pair <int, int>
+#define pb push_back
+#define pob pop_back
+#define f first
+#define s second
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+
+int main() {
+    speedio;
+    int n; cin >> n;
+    string s;
+    cin >> s;
+    int leftCount = 0, rightCount = 0;
+    for (int i = 0; i < n; i++) {
+        if (s[i] == 'L') leftCount++;
+        else rightCount++;
+    }   
+    cout << leftCount + rightCount + 1 << nl;
+}
