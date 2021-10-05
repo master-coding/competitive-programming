28ac7ed0a65671a7b838422f061ef0e2cc83a030
Aaris Ahmed <aaris.ahmed1999@gmail.com>
2021-10-05 18:44:48 +0530

    A_Design_Tutorial_Learn_from_Math: create

---
+++ codeforces/A_Design_Tutorial_Learn_from_Math.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define speedio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define tlp int tc; cin >> tc; while (tc--)
+#define rep(i, a, b) for(int i = a; i < b; ++i)
+#define sp ' '
+#define nl '\n'
+#define newline cout << '\n'
+#define ll long long int
+
+#define vi vector <int>
+#define pii pair <int, int>
+#define pb push_back
+#define pob pop_back
+
+
+
+int main() {
+    speedio;
+    
+    int n;
+    cin >> n;
+
+    if (n % 2 == 0)
+        cout << 4 << sp << n - 4 << nl;
+    else
+        cout << 9 << sp << n - 9 << nl;
+}
