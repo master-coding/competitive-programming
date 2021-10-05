4d78af68bd31f4290933a08a732667eaca97cfd5
Aaris Ahmed <aaris.ahmed1999@gmail.com>
2021-10-05 16:05:15 +0530

    50A A_Domino_piling: create

---
+++ codeforces/A_Domino_piling.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define speedio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define tlp int tc; cin >> tc; while (tc--)
+#define rep(i, a, b) for(int i = a; i < b; ++i)
+#define sp ' '
+#define nl '\n'
+#define ll long long int
+
+
+int  main() {
+    speedio;
+    int m, n;
+    cin >> m >> n;
+    cout << (m * n)/2 << nl;
+}
