6a1a7c78fb21f368cbf53756e70779b11a2e5ecd
Aaris Ahmed <aaris.ahmed1999@gmail.com>
2021-10-05 12:19:59 +0530

    71A A_Way_Too_Long_Words: create

---
+++ codeforces/A_Way_Too_Long_Words.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define speedio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define tlp int tc; cin >> tc; while (tc--)
+#define rep(i, a, b) for(int i = a; i < b; ++i)
+#define sp ' '
+#define nl '\n'
+#define newline cout << '\n'
+#define ll long long int
+
+#define vi vector <int>
+#define pii pair <int, int>
+#define pb push_back
+#define pob pop_back
+#define f first
+#define s second
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+
+int main() {
+    speedio;
+    tlp {
+        string s;
+        cin >> s;
+        if (s.length() <= 10) cout << s << nl;
+        else {
+            cout << s[0] << s.length() - 2 << s[s.length() - 1] << nl;
+        }
+    }   
+}
