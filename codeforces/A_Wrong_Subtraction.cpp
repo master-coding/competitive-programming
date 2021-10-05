db4aceda8be2e6a0b8d7ff3a9f20c74c7175c4fc
Aaris Ahmed <aaris.ahmed1999@gmail.com>
2021-10-05 15:59:49 +0530

    977A_Wrong_Subtraction: create

---
+++ codeforces/A_Wrong_Subtraction.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define speedio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define wlp(tc) while(tc--)
+#define flp(i, a, b) for(int i = a; i < b; i++)
+#define sp ' '
+#define nl '\n'
+#define ll long long int
+
+
+int  main() {
+    speedio;
+    int num, k;
+    cin >> num >> k;
+    wlp(k) {
+        int last = num % 10;
+        if (last > 0)
+            num = num - 1;
+        else
+            num = num / 10;           
+    }
+    cout << num;
+
+}
