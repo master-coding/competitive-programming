d4decd87ca952746282cf30895b90faa084a864f
Aaris Ahmed <aaris.ahmed1999@gmail.com>
2021-10-05 17:06:45 +0530

    1525A A_Potion_making: create

---
+++ codeforces/A_Potion_making.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define speedio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define tlp int tc; cin >> tc; while (tc--)
+#define rep(i, a, b) for(int i = a; i < b; ++i)
+#define sp ' '
+#define nl '\n'
+#define newline cout << '\n'
+#define ll long long int
+
+#define vi vector <int>
+
+
+
+int main() {
+    speedio;
+    tlp {       
+        int k;
+        cin >> k;
+        cout << 100/__gcd(k, 100) << nl;
+    }
+}
