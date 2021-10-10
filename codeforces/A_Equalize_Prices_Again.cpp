557e36022575ff5c7535efc9ce432d055b445a6a
Aaris Ahmed <aaris.ahmed1999@gmail.com>
2021-10-10 20:39:46 +0530

    1234A A_Equalize_Prices_Again: create

---
+++ codeforces/A_Equalize_Prices_Again.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define speedio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define tlp int tc; cin >> tc; while (tc--)
+#define rep(i, a, b) for(int i = a; i < b; ++i)
+#define sp ' '
+#define nl '\n'
+#define newline cout << '\n'
+#define ll long long int
+
+#define vi vector <int>
+#define pii pair <int, int>
+#define pb push_back
+#define pob pop_back
+#define f first
+#define s second
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+
+int main() {
+    speedio;
+    tlp {
+        int n; cin >> n;
+        int arr[n];
+        ll sum = 0;
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+            sum += arr[i];
+        }
+        cout << fixed << setprecision(0) << ceil((double)sum / n) << nl;
+    }   
+}
