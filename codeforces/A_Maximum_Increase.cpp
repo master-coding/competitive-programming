a61a8557d31a86ba9b25cb500193b7a86ba1997b
Aaris Ahmed <aaris.ahmed1999@gmail.com>
2021-10-19 21:01:59 +0530

    702A_Maximum_Increase: create

---
+++ codeforces/A_Maximum_Increase.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define speedio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define tlp int tc; cin >> tc; while (tc--)
+#define rep(i, a, b) for(int i = a; i < b; ++i)
+#define sp ' '
+#define nl '\n'
+#define newline cout << '\n'
+#define ll long long int
+
+#define vi vector <int>
+#define pii pair <int, int>
+#define pb push_back
+#define pob pop_back
+#define f first
+#define s second
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+
+int main() {
+    speedio;
+    int n; cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int ans = 1, count = 1;
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > arr[i - 1]) {
+            count++;
+            ans = max(ans, count);
+        }
+        else {
+            count = 1;
+        }
+    }
+    cout << ans << nl;   
+}
