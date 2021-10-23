0d8423004d5bb6b7252d8bfe24af80293813c3fc
Aaris Ahmed <aaris.ahmed1999@gmail.com>
2021-10-23 09:05:07 +0530

    1255A A_Changing_Volume: create

---
+++ codeforces/A_Changing_Volume.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define speedio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define tlp int tc; cin >> tc; while (tc--)
+#define rep(i, a, b) for(int i = a; i < b; ++i)
+#define sp ' '
+#define nl '\n'
+#define newline cout << '\n'
+#define ll long long int
+
+#define vi vector <int>
+#define pii pair <int, int>
+#define pb push_back
+#define pob pop_back
+#define f first
+#define s second
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+
+int main() {
+    speedio;
+    tlp {
+        int a, b;
+        cin >> a >> b;
+        int diff = max(a, b) - min(a, b);
+        int ans = 0;
+        int arr[3] = {5, 2, 1};
+        for (int i = 0; i < 3; i++) {
+            ans += diff / arr[i];
+            diff %= arr[i];
+        }
+        cout << ans << nl;
+    }
+}
