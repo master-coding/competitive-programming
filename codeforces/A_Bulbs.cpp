417079e13d76e9a6604e9dea65723c15d89d75e0
Aaris Ahmed <aaris.ahmed1999@gmail.com>
2021-10-31 19:26:21 +0530

    615A A_Bulbs: create

---
+++ codeforces/A_Bulbs.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define speedio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define tlp int tc; cin >> tc; while (tc--)
+#define rep(i, a, b) for(int i = a; i < b; ++i)
+#define sp ' '
+#define nl '\n'
+#define newline cout << '\n'
+#define ll long long int
+
+#define vi vector <int>
+#define pii pair <int, int>
+#define pb push_back
+#define pob pop_back
+#define f first
+#define s second
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+
+int main() {
+    speedio;
+    int n, m;
+    cin >> n >> m;
+    set <int> s;
+    for (int i = 0; i < n; i++) {
+        int x; cin >> x;
+        for (int i = 0; i < x; i++) {
+            int y; cin >> y;
+            s.insert(y);
+        }
+    }
+    if (m - s.size() == 0) yes;
+    else no;  
+}
