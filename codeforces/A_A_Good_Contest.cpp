fb57cdeaa7c9c282dec1fe86ed948a89e1ea0bd2
Aaris Ahmed <aaris.ahmed1999@gmail.com>
2021-11-19 20:41:28 +0530

    681A A_A_Good_Contest: create

---
+++ codeforces/A_A_Good_Contest.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define speedio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define tlp int tc; cin >> tc; while (tc--)
+#define rep(i, a, b) for(int i = a; i < b; ++i)
+#define sp ' '
+#define nl '\n'
+#define newline cout << '\n'
+#define ll long long int
+
+#define vi vector <int>
+#define pii pair <int, int>
+#define pb push_back
+#define pob pop_back
+#define f first
+#define s second
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+
+int main() {
+    speedio;
+    int n; cin >> n;
+    bool flag = false;
+    for (int i = 0; i < n; i++) {
+        string s; int a, b;
+        cin >> s >> a >> b;
+        if (a >= 2400 and b > a) flag = true;
+    }
+    if (flag) yes;
+    else no;
+}
