8670395f2e461e809fa9ecc9a7624714865d2697
Aaris Ahmed <aaris.ahmed1999@gmail.com>
2021-12-05 17:01:03 +0530

    1447A A_Add_Candies: create

---
+++ codeforces/A_Add_Candies.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define speedio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define tlp int tc; cin >> tc; while (tc--)
+#define rep(i, a, b) for(int i = a; i < b; ++i)
+#define sp ' '
+#define nl '\n'
+#define newline cout << '\n'
+#define ll long long int
+
+#define vi vector <int>
+#define pii pair <int, int>
+#define pb push_back
+#define pob pop_back
+#define f first
+#define s second
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+
+int main() {
+    speedio;
+    tlp {
+        int n; cin >> n;
+        cout << n << nl;
+        for (int i = 1; i <= n; i++) {
+            cout << i << ' ';
+        }
+        newline;
+    }   
+}
