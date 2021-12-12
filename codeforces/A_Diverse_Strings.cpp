c0f09f12c552b7614d40213970ff4ca90269209a
Aaris Ahmed <aaris.ahmed1999@gmail.com>
2021-12-12 21:33:15 +0530

    1144A A_Diverse_Strings: create

---
+++ codeforces/A_Diverse_Strings.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define speedio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define tlp int tc; cin >> tc; while (tc--)
+#define rep(i, a, b) for(int i = a; i < b; ++i)
+#define sp ' '
+#define nl '\n'
+#define newline cout << '\n'
+#define ll long long int
+
+#define vi vector <int>
+#define pii pair <int, int>
+#define pb push_back
+#define pob pop_back
+#define f first
+#define s second
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+
+int main() {
+    speedio;
+    tlp {
+        string x; cin >> x;
+        sort(x.begin(), x.end());
+
+        bool flag = true;
+        for (int i = 1; i < x.length(); i++) {
+            if (x[i] - x[i - 1] != 1) {
+                flag = false;
+                break;
+            }
+        }
+        if (flag) yes;
+        else no;
+    }   
+}
