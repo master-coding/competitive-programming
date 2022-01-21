e42426d741cb9803a0c3fa7c82917fd3e96ee336
Aaris Ahmed <aaris.ahmed1999@gmail.com>
2022-01-21 22:18:52 +0530

    451A A_Game_With_Sticks: create

---
+++ codeforces/A_Game_With_Sticks.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define speedio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define tlp int tc; cin >> tc; while (tc--)
+#define nl '\n'
+#define newline cout << '\n'
+#define ll long long int
+
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+
+int main() {
+    speedio;
+    int n, m; cin >> n >> m;
+    if (min(n, m) % 2) cout << "Akshat" << nl;
+    else cout << "Malvika" << nl;   
+}
