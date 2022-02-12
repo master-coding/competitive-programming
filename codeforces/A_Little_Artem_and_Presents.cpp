0adc760457cc6b1f6ae5b575f3e90353da2b4ee4
Aaris Ahmed <aaris.ahmed1999@gmail.com>
2022-02-12 19:06:53 +0530

    669A A_Little_Artem_and_Presents: create

---
+++ codeforces/A_Little_Artem_and_Presents.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define speedio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define tlp int tc; cin >> tc; while (tc--)
+#define nl '\n'
+#define newline cout << '\n'
+#define ll long long int
+
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+
+int main() {
+    speedio;
+    int n; cin >> n;
+    cout << 2 * (n / 3) + (n % 3 == 0 ? 0 : 1) << nl;   
+}
