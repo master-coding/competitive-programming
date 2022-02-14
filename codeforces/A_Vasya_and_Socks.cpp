66efab1d4f364492205939980d89e93d36484c97
Aaris Ahmed <aaris.ahmed1999@gmail.com>
2022-02-14 21:06:04 +0530

    460A A_Vasya_and_Socks: create

---
+++ codeforces/A_Vasya_and_Socks.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define speedio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define tlp int tc; cin >> tc; while (tc--)
+#define nl '\n'
+#define newline cout << '\n'
+#define ll long long int
+
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+
+int main() {
+    speedio;
+    int n, m;
+    cin >> n >> m;
+    cout << n + ((n - 1) / (m - 1));
+}
