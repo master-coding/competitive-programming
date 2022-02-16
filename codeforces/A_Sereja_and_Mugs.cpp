744b8a43cd27821f0f221924c9452c698fc7ef08
Aaris Ahmed <aaris.ahmed1999@gmail.com>
2022-02-16 21:07:34 +0530

    42A_Sereja_and_Mugs: create

---
+++ codeforces/A_Sereja_and_Mugs.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define speedio ios_base::sync_with_stdio(false); cin.tie(NULL);
+#define tlp int tc; cin >> tc; while (tc--)
+#define nl '\n'
+#define newline cout << '\n'
+#define ll long long int
+
+#define yes cout << "YES" << nl
+#define no cout << "NO" << nl
+
+int main() {
+    speedio;
+    int n, k; cin >> n >> k;
+    int arr[n];
+    for (int i = 0; i < n; i++) cin >> arr[i];
+
+    sort(arr, arr + n);   
+    int sum = accumulate(arr, arr + n, 0);
+    sum -= arr[n - 1];
+    if (sum <= k) yes;
+    else no;
+}
